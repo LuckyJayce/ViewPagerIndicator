5428b54cfcb18701e6267d2bb498bdffde3b7486
LuckyJayce <794629068@qq.com>
2016-07-30 08:58:32 +0800

    1

--- app/src/main/java/com/shizhefei/indicator/BannerActivity.java
+++ app/src/main/java/com/shizhefei/indicator/BannerActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -20,7 +19,6 @@ import com.shizhefei.view.viewpager.RecyclingPagerAdapter;
 public class BannerActivity extends Activity {
 
     private BannerComponent bannerComponent;
-    private LayoutInflater inflate;
 
     @Override
     protected void onCreate(Bundle arg0) {
@@ -33,7 +31,6 @@ public class BannerActivity extends Activity {
         viewPager.setOffscreenPageLimit(2);
 
         bannerComponent = new BannerComponent(indicator, viewPager, false);
-        inflate = LayoutInflater.from(getApplicationContext());
         bannerComponent.setAdapter(adapter);
 
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
